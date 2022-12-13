43f18768bcaee54d2027f9a714bd94a7928c8b36
Ashok Timsina <64159472+mrfreakish@users.noreply.github.com>
2022-12-13 23:18:03 +0400

    updated movingaverage.ino

--- movingaverage.ino
+++ movingaverage.ino
@@ -3,9 +3,9 @@ using namespace std;
 float x, y, z;
 void setup(){M5.begin();
 M5.IMU.Init( );
-Serial.begin(9600);}
+Serial.begin(9600);} // initializing serial monitor
 
-#define WINDOW_SIZE 8
+#define WINDOW_SIZE 8 // we are using window size of 8 for moving average
 
 int I = 0;
 float sumx = 0, sumy = 0, sumz = 0;
@@ -18,12 +18,14 @@ float zAveraged = 0;
 
 void loop(){
   
+  // keeping track of the sum of required values for moving average
   sumx = sumx - xValues[I];
   sumy = sumy - yValues[I];
   sumz = sumz - zValues[I];
 
-  M5.IMU.getAccelData(&x, &y, &z);
+  M5.IMU.getAccelData(&x, &y, &z); // getting value form IMU
   
+  // updating old value with new value for moving average 
   xValues[I] = x;
   yValues[I] = y;
   zValues[I] = z;
@@ -36,7 +38,7 @@ void loop(){
   yAveraged = sumy / WINDOW_SIZE;
   zAveraged = sumz / WINDOW_SIZE; 
 
-  I = (I+1) % WINDOW_SIZE;
+  I = (I+1) % WINDOW_SIZE; // the most important step where we get the index of oldest data which will be removed for new moving average
 
   Serial.print(xAveraged);
   Serial.print(" ");
